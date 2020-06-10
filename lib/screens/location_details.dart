071eda563127f87d289a74a9211615b00bc481ff
frankbo <frank.boeddeker@gmx.net>
2020-06-10 13:55:05 +0200

    Show hero image and location name

--- lib/screens/location_details.dart
+++ lib/screens/location_details.dart
@@ -5,16 +5,17 @@ import 'package:kicker_app/widgets/location_detail.dart';
 class LocationDetailsPage extends StatelessWidget {
   static final String route = "/location-details";
 
-  final Location location;
-  LocationDetailsPage(this.location);
+  final Location arguments;
+  const LocationDetailsPage(this.arguments);
 
   @override
   Widget build(BuildContext context) {
+    print(this.arguments);
     return Scaffold(
         appBar: AppBar(
           title: Center(child: Text("KickerApp")),
           centerTitle: true,
         ),
-        body: LocationDetail(this.location));
+        body: LocationDetail(this.arguments));
   }
 }
--- lib/widgets/location_detail.dart
+++ lib/widgets/location_detail.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kicker_app/models/location.dart';
+import 'package:kicker_app/static/kicker-colors.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 const googleMapsUrl = 'https://www.google.com/maps/';
 
-Future<Null> Function() _launchURL(Location location) => () async {
-      String googleUrl = googleMapsUrl +
-          'search/?api=1&query=${location.loc.latitude},${location.loc.longitude}';
-      if (await canLaunch(googleUrl)) {
-        await launch(googleUrl);
-      } else {
-        throw 'Could not launch $googleUrl';
-      }
-    };
+_launchURL(Location location) async {
+  String googleUrl = googleMapsUrl +
+      'search/?api=1&query=${location.loc.latitude},${location.loc.longitude}';
+  if (await canLaunch(googleUrl)) {
+    await launch(googleUrl);
+  } else {
+    throw 'Could not launch $googleUrl';
+  }
+}
+
+Widget _heroIcon() {
+  return Stack(children: [
+    ColorFiltered(
+        colorFilter: ColorFilter.mode(
+          Colors.grey,
+          BlendMode.saturation,
+        ),
+        child: Opacity(
+          opacity: 0.6,
+          child:
+              Image.asset('assets/images/kicker-table.png', fit: BoxFit.fill),
+        )),
+    Positioned.fill(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.camera_alt,
+          size: 40,
+        ),
+        Text(
+          "Kein Foto",
+          textAlign: TextAlign.center,
+        )
+      ],
+    ))
+  ]);
+}
+
+Widget _overView(Location location) {
+  return Container(
+      color: KickerColors.main,
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [Text(location.name), Text("xxx km")],
+      ));
+}
 
 class LocationDetail extends StatelessWidget {
   final Location location;
@@ -21,11 +61,19 @@ class LocationDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: _launchURL(location),
-        child: Text('Bring me to the location'),
-      ),
+    return ListView(
+      children: [
+        _heroIcon(),
+        _overView(this.location),
+        Center(
+          child: RaisedButton(
+            onPressed: () {
+              _launchURL(location);
+            },
+            child: Text('Bring me to the location'),
+          ),
+        ),
+      ],
     );
   }
 }
