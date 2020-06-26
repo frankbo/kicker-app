e630c1b5ab2f8288cfd907833e27b58ff605db13
frankbo <frank.boeddeker@gmx.net>
2020-06-26 21:05:43 +0200

    Add description field and static map with link to location

--- lib/widgets/location_detail.dart
+++ lib/widgets/location_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kicker_app/models/location.dart';
+import 'package:kicker_app/static/keys.dart';
 import 'package:kicker_app/static/kicker-colors.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -47,12 +48,43 @@ Widget _heroIcon() {
 
 Widget _overview(Location location) {
   return Container(
-      color: KickerColors.main,
-      padding: EdgeInsets.all(10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Text(location.name), Text("xxx km")],
-      ));
+    color: KickerColors.main,
+    padding: EdgeInsets.all(10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [Text(location.name), Text("xxx km")],
+    ),
+  );
+}
+
+Widget _descriptionArea(Location location) {
+  return Container(
+    padding: EdgeInsets.all(10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Text(
+              "Some Description ${location.name} about the location: price, table, tableStatus"),
+        )
+      ],
+    ),
+  );
+}
+
+Widget _staticMap(Location location) {
+  final mapStyle = "osm-carto";
+  final width = "600";
+  final height = "200";
+  final apiBaseUrl = "https://maps.geoapify.com";
+  return GestureDetector(
+    onTap: () {
+      _launchURL(location);
+    },
+    child: Image.network(
+      "$apiBaseUrl/v1/staticmap?style=$mapStyle&width=$width&height=$height&center=lonlat:${location.loc.longitude},${location.loc.latitude}&zoom=14&marker=lonlat:${location.loc.longitude},${location.loc.latitude};color:%23ff0000;size:medium&apiKey=${Keys.geoapifyKey}",
+    ),
+  );
 }
 
 class LocationDetail extends StatelessWidget {
@@ -65,6 +97,8 @@ class LocationDetail extends StatelessWidget {
       children: [
         _heroIcon(),
         _overview(this.location),
+        _descriptionArea(this.location),
+        _staticMap(this.location),
         Center(
           child: RaisedButton(
             onPressed: () {
